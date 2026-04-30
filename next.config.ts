efd9c663ff947882c409be8c787465ce4f525738
donghyeon <ehdgud6634@gmail.com>
2026-04-30 20:59:35 +0900

    csv 로 바꿈

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  // @ts-expect-error: Next.js dev server suggests this option but it might not be in the type definitions yet
   allowedDevOrigins: ['192.168.219.*'],
 };
 
